31c76f5a5bedb679e3e54123c93253a6d8ce4802
Nurmayda Manalu <moonlunaa@gmail.com>
2024-08-01 19:23:38 +0800

    config tugas

--- js/script.js
+++ js/script.js
@@ -14,8 +14,31 @@ function formValidation() {
     }
 }
 
+let indexSlide = 1; // [0, 1, 2]
 showSlide();
+
+nextSlide(1)
+
+function nextSlide(n) {
+    showSlide(indexSlide += n);
+}
+
 function showSlide() {
     let listImage = document.getElementsByClassName('main-content-banner');
     console.log(listImage);
+
+    // Algoritma untuk mereset index dari slidenya
+    if (n >listImage.length) indexSlide = 1;
+
+    // Algoritma untuk mengilangkan semua gambar
+    let index = 0;
+    while (index < listImage.length) {
+        listImage [index].style.display = 'none';
+        index++;
+    }
+    // Algoritma untuk memunculkan 1 gambar saja
+    listImage[indexSlide - 1].style.display = 'block';
 }
+
+Algoritma untuk auto slide
+setInterval(() => nextSlide(1), 3000);
