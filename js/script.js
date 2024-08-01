4c2940a328a8c0cae8964fedd8c774c3eee8e878
Nurmayda Manalu <moonlunaa@gmail.com>
2024-08-01 18:40:47 +0800

    config tugas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 /* Ini Javascript */
 
-console.log(Hello World);
+function formValidation() {
+    let name = document.getElementById('input-name').value;
+    console.log('name');
+
+    // Pengecekan dimana name tidak boleh kosong
+    if (name == '') {
+        // Code ini akan di eksekusi ketika name = ''
+        alert('Maaf inputan anda kosong');
+    } else {
+        // Code ini akan di eksekusi ketika name tidak ''
+        alert ('Sukses Menginput')
+    }
+}
+
+showSlide();
+function showSlide() {
+    let listImage = document.getElementsByClassName('main-content-banner');
+    console.log(listImage);
+}
